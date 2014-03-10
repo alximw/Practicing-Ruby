3ce28d04584280effef2bfdb058011c1c074d678
Alximw <alximw@gmail.com>
2014-03-11 00:17:21 +0100

    First commit. Hello world!

---
+++ HelloWorld.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby -w
+
+
+# this is a sigle line comment
+
+=begin
+	
+This is a multi line comment
+
+
+This programs only says hello to everyone!
+	
+=end
+
+
+puts "Hello world!"
